78b1e1cfff620d678d6be2fcf76f24fe0858cba8
sohana08 <sohana0808@gmail.com>
2023-02-13 23:45:34 +0600

    :shirt: removed unneccesory comments

--- pdfsplit.js
+++ pdfsplit.js
@@ -4,6 +4,7 @@ const s3 = new AWS.S3()
 // const pdf = require("pdf-page-counter")
 // const fs = require("fs")
 const { PDFDocument } = require("pdf-lib")
+const { pdf2img } = require("pdf2img")
 
 exports.handler = async (event) => {
   const srcBucket = event.Records[0].s3.bucket.name
@@ -35,27 +36,6 @@ exports.handler = async (event) => {
     const origiPdf = await s3.getObject(params).promise()
 
     const pdfDoc = await PDFDocument.load(origiPdf.Body)
-    // const promises
-
-    // const pages = pdfDoc.getPages()
-    // const firstPage = pages[0]
-    // // Create a new "sub" document
-    // const subDocument = await PDFDocument.create()
-    // const copiedPage = await subDocument.copyPages(pdfDoc, firstPage)
-    // subDocument.addPage(copiedPage)
-    // const pdfBytes = await subDocument.save()
-    // const splitFile = srcKey.split(".pdf")[0] + "_" + i + ".pdf"
-
-    // await s3
-    //   .putObject({
-    //     Bucket: dstBucket,
-    //     Key: splitFile,
-    //     Body: pdfBytes,
-    //   })
-    //   .promise()
-
-    // for (let i = 0; i < pdfDoc.getPages().length; i++) {
-    // Create a new "sub" document
     const subDocument = await PDFDocument.create()
 
     // copy the page at current index
@@ -63,73 +43,29 @@ exports.handler = async (event) => {
     subDocument.addPage(copiedPage)
     const pdfBytes = await subDocument.save()
 
-    const splitFile = dstKey.split(".pdf")[0] + ".pdf"
-    // promises.push(
+    const splitFile = dstKey.split(".pdf")[0]
+
+    console.log("PDF file split and saved to S3 successfully.")
+
+    pdf2img.setOptions({
+      type: "png",
+      density: 600, // default 600
+      outputdir: dstBucket, // output folder, default null (if null given, then it will create folder name same as file name)
+      outputname: splitFile, // output file name, dafault null (if null given, then it will create image name same as input name)
+    })
+
+    pdf2img.convert(pdfBytes, function (err, info) {
+      if (err) console.log(err)
+      else console.log(info)
+    })
+
     await s3
       .putObject({
         Bucket: dstBucket,
         Key: splitFile,
-        Body: pdfBytes,
+        Body: imageFile,
       })
       .promise()
-    // .promise()
-    // )
-    // }
-
-    // const response = await Promise.all(promises)
-    console.log("PDF file split and saved to S3 successfully.")
-    return {
-      message: "PDF file split and saved to S3 successfully.",
-    }
-
-    // const options = {
-    //   density: 100,
-    //   saveFilename: dstBucket,
-    //   savePath: dstKey,
-    //   format: "png",
-    //   width: 400,
-    //   height: 400,
-    // }
-    // console.log("image", origimage)
-    // // const image = await Sharp(origimage.Body).metadata()
-    // console.log("image2", origimage.Body)
-    // const storeAsImage = await fromPath(srcKey, options)
-    // const pageToConvertAsImage = 1
-
-    // storeAsImage(pageToConvertAsImage).then((resolve) => {
-    //   console.log("Page 1 is now converted as image")
-
-    //   return resolve
-    // })
-    // console.log("image3", storeAsImage)
-
-    // // console.log(image)
-    // // Use the sharp module to resize the image and save in a buffer.
-    // const resizeParams = {
-    //   width: 200,
-    //   height: 200,
-    //   fit: Sharp.fit.cover,
-    //   position: Sharp.strategy.entropy,
-    // }
-
-    // console.log("re", resizeParams)
-
-    // if (image.width > image.height) {
-    //   resizeParams.width = null
-    // } else {
-    //   resizeParams.height = null
-    // }
-
-    // const buffer = await Sharp(storeAsImage).resize(resizeParams).toBuffer()
-
-    // const destparams = {
-    //   Bucket: dstBucket,
-    //   Key: dstKey,
-    //   Body: buffer,
-    //   ContentType: "image",
-    // }
-
-    // const putResult = await s3.putObject(destparams).promise()
   } catch (error) {
     console.log(error)
     return
